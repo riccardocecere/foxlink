962aaaf89dc0393c033115144e64d7752b5dc667
Unknown <fabio.raschini@gmail.com>
2018-06-27 12:54:23 +0200

    Improved warcs discovery

--- common_crawl_discovery.py
+++ common_crawl_discovery.py
@@ -1,23 +1,39 @@
 from pyspark import SparkContext, SparkConf
-from web_discovery_aux import common_crawl_aux
+from web_discovery_aux import common_crawl_aux,parsing_aux
+
 
 # Set context
 conf = SparkConf().setAppName('product_finder')
 sc = SparkContext(conf=conf)
 
-# Take input from file on hdfs
-path = 'hdfs:///user/maria_dev/data/siti_dexter.txt'
+# Take inputs from file on hdfs
+path = 'hdfs:///user/maria_dev/data/lista_siti_prodotti_dexter.txt'
+path_common_crawl = 'hdfs:///user/maria_dev/data/common_crawl_snapshots.txt'
+
 input = sc.textFile(path)
+common_crawl_snapshots = sc.textFile(path_common_crawl)
 
 
+print '---------------------------------------'
+print(common_crawl_snapshots)
+print '---------------------------------------'
+
+# cartesian1 to create a new rdd with the cartesian product betweeen input and common_crawl_snapshots, in the form (url, snapshotname)
 # map 1 for every url it makes a query to the common crawl repository ----> (url, response)
 # flatMap 1 for every warcs returned in the response it will create a key associate with the url value, it creates couple with (warcname, url)
+# filter1 filters out all the warc name with the incorrect name, since they are not useful for analysis
 # map 2 transform the value in a json with two keys, the length of the list of urls associate with a single warcfile and the list itself
-warc_urls = input.map(lambda url:common_crawl_aux.warc_discovery(url))\
+# sort for the num_of_urls
+warc_urls = input.cartesian(common_crawl_snapshots)\
+    .map(lambda (url,snapshot):common_crawl_aux.warc_discovery(url,snapshot))\
     .flatMap(lambda (url,response): (common_crawl_aux.id_warc_selection(url,single_response) for single_response in response))\
+    .filter(lambda (warcname,url): parsing_aux.correct_warc_name(warcname))\
     .groupByKey()\
     .mapValues(list)\
-    .map(lambda (warc_file_name,urls):(warc_file_name,{'num_of_urls':len(urls),'urls':urls}))
+    .map(lambda (warc_file_name,urls):(warc_file_name,{'num_of_urls':len(urls),'urls':urls}))\
+    .sortBy(lambda (warc_file_name,dict_urls): desc(dict_urls['num_of_urls']))
+
+
 
 warc_urls.persist()
 
--- web_discovery_aux/parsing_aux.py
+++ web_discovery_aux/parsing_aux.py
@@ -1,6 +1,10 @@
 import re
+import requests
+import pattern.web
+
 
 # Function that takes id and a result in a response to generate  (domain, (page,id))
+
 def find_domain_url(id, result):
 
     # Check the id
@@ -17,3 +21,28 @@ def find_domain_url(id, result):
 
     except:
         return (id,'ERROR://parsing_problem')
+
+
+#function to extract clean text from html pages
+def extract_clean_text_from_page(url):
+    try:
+        html = requests.get(str(url)).text
+    except:
+        print 'error to take the page: '+str(url)
+        return 'ERROR:to take the page'
+    try:
+        result = pattern.web.plaintext(html).encode('utf-8','ignore')
+    except:
+        print 'error extracting plain text from page: '+str(url)
+        return 'ERROR: extracting plainj text'
+    print 'text extracted from page: '+str(url)
+    return result
+
+# Function to select if a warc is useful based on it's name that must be in the form crawl-data/CC-MAIN-2018-22/segments/1526794863689.50/warc/CC-MAIN-20180520205455-20180520225455-00609.warc.gz
+# it's important that has /warc/ inside the string
+
+def correct_warc_name(warcname):
+    if warcname == '' or warcname == None:
+        return False
+    return re.match('crawl.*\/warc\/.*gz',warcname)
+
