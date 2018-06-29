dfd0e79df9b31d7f1a0ec7a160a66325fb60e97b
Unknown <fabio.raschini@gmail.com>
2018-06-29 11:59:53 +0200

    Input CSV
    - Take csv input
    - Descent order
    - Calculate total number of warcs
    - Fix comments

--- common_crawl_discovery.py
+++ common_crawl_discovery.py
@@ -7,39 +7,42 @@ conf = SparkConf().setAppName('product_finder')
 sc = SparkContext(conf=conf)
 
 # Take inputs from file on hdfs
-path = 'hdfs:///user/maria_dev/data/lista_siti_prodotti_dexter.txt'
+path = 'hdfs:///user/maria_dev/data/cameras.csv'
 path_common_crawl = 'hdfs:///user/maria_dev/data/common_crawl_snapshots.txt'
 
-input = sc.textFile(path)
+site_list = sc.textFile(path)
 common_crawl_snapshots = sc.textFile(path_common_crawl)
 
-
-print '---------------------------------------'
-print(common_crawl_snapshots)
-print '---------------------------------------'
+#Remove the header from csv and remove the ',' at the end of each url
+header = site_list.first()
+site_list = site_list.filter(lambda row: row != header)\
+    .map(lambda row: row[:-1])
 
 # cartesian1 to create a new rdd with the cartesian product betweeen input and common_crawl_snapshots, in the form (url, snapshotname)
 # map 1 for every url it makes a query to the common crawl repository ----> (url, response)
-# flatMap 1 for every warcs returned in the response it will create a key associate with the url value, it creates couple with (warcname, url)
-# filter1 filters out all the warc name with the incorrect name, since they are not useful for analysis
-# map 2 transform the value in a json with two keys, the length of the list of urls associate with a single warcfile and the list itself
-# sort for the num_of_urls
-warc_urls = input.cartesian(common_crawl_snapshots)\
+# map 2 for every warcs returned in the response it will create a key associate with the url value, it creates couple with (url, warcs)
+# reduceByKey aggregate all the same keys (url) and combine the list of warcs (making the set)
+# map 3 transform the value in a json with two keys, the length of the list of warc name associate with a single url and the list itself
+# filter out all the value with less than 1 num_of_warcs, it means that the url is not on common crawl, or there were some errors
+# sort for the num_of_warcs
+url_warcs = site_list.cartesian(common_crawl_snapshots)\
     .map(lambda (url,snapshot):common_crawl_aux.warc_discovery(url,snapshot))\
-    .flatMap(lambda (url,response): (common_crawl_aux.id_warc_selection(url,single_response) for single_response in response))\
-    .filter(lambda (warcname,url): parsing_aux.correct_warc_name(warcname))\
-    .groupByKey()\
-    .mapValues(list)\
-    .map(lambda (warc_file_name,urls):(warc_file_name,{'num_of_urls':len(urls),'urls':urls}))\
-    .sortBy(lambda (warc_file_name,dict_urls): desc(dict_urls['num_of_urls']))
+    .map(lambda (url,response): common_crawl_aux.id_multiple_warc_selection(url,response)) \
+    .mapValues(list) \
+    .reduceByKey(lambda warcs1,warcs2: list(set(list(warcs1+warcs2))))\
+    .map(lambda (url, warcs): (url, {'num_of_warcs': len(warcs), 'warc_names': warcs}))\
+    .filter(lambda (url,warcs):warcs['num_of_warcs']>0)\
+    .sortBy((lambda (url,dict_warcs): dict_warcs['num_of_warcs']),False)
 
+#Calculate the sum of warcs necessary to be downloaded
+count_warcs = url_warcs
+count_warcs = count_warcs.values()\
+    .map(lambda dict_warcs: dict_warcs['num_of_warcs'])\
+    .sum()
 
-
-warc_urls.persist()
-
-
-print('NUMBER OF WARC FILE FOUND: '+str(warc_urls.count()))
-
-warc_urls.saveAsTextFile('hdfs:///user/maria_dev/data/results')
+print '------------------------'
+print 'Total number of warcs: '+str(count_warcs)
+print '------------------------'
+url_warcs.saveAsTextFile('hdfs:///user/maria_dev/data/results')
 
 sc.stop()
